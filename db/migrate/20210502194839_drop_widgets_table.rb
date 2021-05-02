fd92592b637482ed6d660e72a65ab4196a9c8b6f
RonnieHoffer <ronnie.hoffer@gmail.com>
2021-05-02 19:32:02 -0400

    Widgets deleted, one column deleted from Locations

--- db/migrate/20210502194839_drop_widgets_table.rb
+++ db/migrate/20210502194839_drop_widgets_table.rb
@@ -1,10 +1,5 @@
 class DropWidgetsTable < ActiveRecord::Migration[5.2]
   def change
-    def up
-      drop_table :widgets
-    end
-
-    def down
-    end
+    drop_table :widgets
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_02_194839) do
+ActiveRecord::Schema.define(version: 2021_05_02_232637) do
 
   create_table "locations", force: :cascade do |t|
     t.text "departments"
@@ -24,18 +24,6 @@ ActiveRecord::Schema.define(version: 2021_05_02_194839) do
     t.string "manufacturer"
     t.decimal "cost"
     t.decimal "quantity"
-  end
-
-  create_table "widgets", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.string "dietary_restrictions"
-    t.integer "salary"
-    t.integer "number_of_kids"
-    t.string "vulnerabilities"
-    t.string "illnesses"
-    t.string "medication"
-    t.string "voting_preferences"
   end
 
 end
